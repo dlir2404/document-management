bfc2455f81e2dff21e9d97aca57c8475ecd2d48c
Dlir <dinhlinh.work24@gmail.com>
2024-08-07 21:22:44 +0700

    going document api

--- src/modules/documents/document.going.controller.ts
+++ src/modules/documents/document.going.controller.ts
@@ -1,8 +1,31 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { CurrentUserId, LeaderAuth, SpecialistAuth } from "src/shared/decorators";
+import { AcceptRequestProcessDto, DenyRequestProcessDto, RequestProcessDto } from "./dtos/income-document.dto";
+import { GoingDocumentService } from "./document.going.service";
 
 @Controller('/going')
 @ApiTags('Going Documents')
 export class GoingDocumentController {
-    
+    constructor(private readonly goingService: GoingDocumentService) {}
+    @Post('request-process')
+    @LeaderAuth()
+    @ApiOperation({ summary: 'Leader request specialist to process the document '})
+    async requestProcess(@Body() body: RequestProcessDto, @CurrentUserId() userId: number) {
+      return this.goingService.requestProcess(body, userId)
+    }
+  
+    @Post('/request-process/accept')
+    @ApiOperation({ summary: 'Specialist accept the request process '})
+    @SpecialistAuth()
+    async acceptRequestProcess(@Body() body: AcceptRequestProcessDto, @CurrentUserId() userId: number) {
+      return this.goingService.acceptRequestProcess(userId, body.documentId)
+    }
+  
+    @Post('/request-process/deny')
+    @ApiOperation({ summary: 'Specialist accept the request process '})
+    @SpecialistAuth()
+    async denyRequestProcess(@Body() body: DenyRequestProcessDto, @CurrentUserId() userId: number) {
+      return this.goingService.denyRequestProcess(userId, body.documentId, body.returnReason)
+    }
 }
--- src/modules/documents/document.going.service.ts
+++ src/modules/documents/document.going.service.ts
@@ -1,6 +1,133 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
+import { RequestProcessDto } from "./dtos/income-document.dto";
+import { GoingDocument, GoingStatus, ProcessTicket, TicketStatus, User, UserRole } from "src/database/models";
 
 @Injectable()
 export class GoingDocumentService {
-    
+    async requestProcess(body: RequestProcessDto, leaderId: number) {
+        const goingDocument = await GoingDocument.findOne({
+            where: {
+                id: body.documentId
+            }
+        })
+
+        if (!goingDocument) throw new NotFoundException("Document not found")
+
+        if (goingDocument.leaderId != leaderId) throw new ForbiddenException('You are not in charge of this document')
+
+        const mainProcessor = await User.findOne({
+            where: {
+                id: body.specialistId,
+                role: UserRole.SPECIALIST
+            }
+        })
+
+        if (!mainProcessor) {
+            throw new NotFoundException('Main processor not found')
+        }
+
+        await ProcessTicket.create({
+            goingDocumentId: body.documentId,
+            mainProcessorId: body.specialistId,
+            deadline: body.deadline,
+            processDirection: body.processDirection,
+        })
+
+        await GoingDocument.update(
+            {
+                status: GoingStatus.ASSIGNMENT_FOR_PROCESS,
+            },
+            {
+                where: { id: body.documentId },
+            }
+        );
+
+        return { result: true }
+    }
+
+    async acceptRequestProcess(specialistId: number, documentId: number) {
+        const specialist = await User.findOne({
+            where: {
+                id: specialistId,
+                role: UserRole.SPECIALIST
+            }
+        })
+
+        if (!specialist) throw new NotFoundException('Specialist not found')
+
+        const document = await GoingDocument.findOne({
+            where: {
+                id: documentId,
+                status: GoingStatus.ASSIGNMENT_FOR_PROCESS,
+            }
+        })
+
+        if (!document) throw new NotFoundException('Document not found or document status not correct')
+
+        const processTicket = await ProcessTicket.findOne({
+            where: {
+                goingDocumentId: documentId,
+                mainProcessorId: specialistId,
+                status: TicketStatus.WAITING
+            }
+        })
+
+        if (!processTicket) throw new NotFoundException('Ticket not found')
+
+        await document.update(
+            {
+                status: GoingStatus.PROCESSING,
+                mainProcessorId: specialistId
+            }
+        )
+
+        await ProcessTicket.update({
+            status: TicketStatus.ACCEPTED
+        }, { where: { id: processTicket.id }})
+
+        return { result: true }
+    }
+
+    async denyRequestProcess(specialistId: number, documentId: number, returnReason: string) {
+        const specialist = await User.findOne({
+            where: {
+                id: specialistId,
+                role: UserRole.SPECIALIST
+            }
+        })
+
+        if (!specialist) throw new NotFoundException('Specialist not found')
+
+        const document = await GoingDocument.findOne({
+            where: {
+                id: documentId,
+                status: GoingStatus.ASSIGNMENT_FOR_PROCESS
+            }
+        })
+
+        if (!document) throw new NotFoundException('Document not found or document status not correct')
+
+        const processTicket = await ProcessTicket.findOne({
+            where: {
+                goingDocumentId: documentId,
+                mainProcessorId: specialistId,
+                status: TicketStatus.WAITING
+            }
+        })
+
+        if (!processTicket) throw new NotFoundException('Ticket not found')
+
+        await document.update(
+            {
+                status: GoingStatus.WAITING_FOR_ASSIGNMENT,
+            }
+        )
+
+        await ProcessTicket.update({
+            status: TicketStatus.REFUSED,
+            returnReason: returnReason
+        }, { where: { id: processTicket.id }})
+
+        return { result: true }
+    }
 }
